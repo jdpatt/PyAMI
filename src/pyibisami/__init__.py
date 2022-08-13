232bdfc3c875759fec1c2b68f03e6285ce3485ab
David Patterson <14203439+jdpatt@users.noreply.github.com>
2022-08-12 20:07:35 -0400

    switch to a src package and auto version off of git tags/setuptools.

--- src/pyibisami/__init__.py
+++ src/pyibisami/__init__.py
@@ -10,10 +10,16 @@ Original Date:   3 July 2012
 Copyright (c) 2012 by David Banas; All rights reserved World wide.
 """
 import logging
+from pkg_resources import DistributionNotFound, get_distribution
 
-__version__ = "4.0.0"
 __date__ = "December 12, 2021"
 __authors__ = "David Banas"
 __copy__ = "Copyright (c) 2014 David Banas"
 
 logging.getLogger("pyibisami").addHandler(logging.NullHandler())
+
+try:
+    __version__ = get_distribution("pyibisami").version
+except DistributionNotFound:
+    # package is not installed
+    pass
